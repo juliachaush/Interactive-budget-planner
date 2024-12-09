6b30abf2609b124d4060f35f8a1f9e857fe7cee7
juliachaush <juliachaush@gmail.com>
2024-12-09 19:18:57 +0100

    add open modal logic

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ import "./styles/main.css";
 import "./styles/variables.css";
 
 console.log("Webpack працюєrrr!");
+
+const closeButton = document.querySelector(".close-button");
+const modalOutcome = document.getElementById("modal");
+const addOutcomeButton = document.querySelector(".outcome-button");
+
+closeButton.addEventListener("click", () => {
+  modalOutcome.style.display = "none";
+});
+
+addOutcomeButton.addEventListener("click", () => {
+  modalOutcome.style.display = "flex";
+});
